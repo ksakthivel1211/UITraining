813cc5bd914547289158ef390368722f602025ba
ksakthivel1211 <125632352+ksakthivel1211@users.noreply.github.com>
2023-03-14 01:13:20 +0530

    blender

--- BlenderJquery/index.js
+++ BlenderJquery/index.js
@@ -9,42 +9,75 @@ $.when(
     const video = b[0];
     const poster = a[0];
 
-    let videoElement = $(`<video class=video-element src=${video.videoUrl} poster=https://www.slashcam.de/images/news/sprite_fright1-16857_PIC1.jpg controls></video>`);
-    $(".movie-area").append(videoElement);
+    // $.ajax({
+    //     type:'GET',
+    //     URL: 'https://mocki.io/v1/4da47fc5-bbf3-4e41-b35f-c88a584bc4b0',
+    //     dataType: 'json',
+    //     success: (obj) => {
+    //         videoJsonMap(obj)
+    //     }
+    // });
+    // function videoJsonMap(video){
 
-    const videoTitle = $(`<h3 class=video-heading></h3>`);
-    videoTitle.append(video.title);
-    $(".movie-area").append(videoTitle);
+    // Start :: Video-Section
 
-    const videoDescription = $(`<p class=videoDetails></p>`);
-    videoDescription.append(video.description);
-    $(".movie-area").append(videoDescription);
-
-    let fragmentComment = $(document.createDocumentFragment());
-    for (const comment of video.comments) {
-
-    const commentCard = $(`<div class=comment-card></div>`);
-    const commentImageWrapper = $(`<div class=imageContainer></div>`);
-    const commentImg = $(`<img class=comment-img src=${comment.image}></img>`)
-    commentImageWrapper.append(commentImg);
-
-    const commentText = $(`<div></div>`);
-
-    const commentHeading = $(`<h2 class=card-heading></h2>`);
-    commentHeading.append(comment.name);
+        let videoElement = $(`<video class=video-element src=${video.videoUrl} poster=https://www.slashcam.de/images/news/sprite_fright1-16857_PIC1.jpg controls></video>`);
+        $(".movie-area").append(videoElement);
     
-    const commentDescription = $(`<p class=card-description></p>`);
-    commentDescription.append(comment.comment);
+        const videoTitle = $(`<h3 class=video-heading></h3>`);
+        videoTitle.append(video.title);
+        $(".movie-area").append(videoTitle);
+    
+        const videoDescription = $(`<p class=videoDetails></p>`);
+        videoDescription.append(video.description);
+        $(".movie-area").append(videoDescription);
 
-    commentText.append(commentHeading);
-    commentText.append(commentDescription);
+    // End :: Video-Section
 
-    commentCard.append(commentImageWrapper);
-    commentCard.append(commentText);
+    // Start :: Comment-Section
+    
+        let fragmentComment = $(document.createDocumentFragment());
+        for (const comment of video.comments) {
+    
+        const commentCard = $(`<div class=comment-card></div>`);
+        const commentImageWrapper = $(`<div class=imageContainer></div>`);
+        const commentImg = $(`<img class=comment-img src=${comment.image}></img>`)
+        commentImageWrapper.append(commentImg);
+    
+        const commentText = $(`<div></div>`);
+    
+        const commentHeading = $(`<h2 class=card-heading></h2>`);
+        commentHeading.append(comment.name);
+        
+        const commentDescription = $(`<p class=card-description></p>`);
+        commentDescription.append(comment.comment);
+    
+        commentText.append(commentHeading);
+        commentText.append(commentDescription);
+    
+        commentCard.append(commentImageWrapper);
+        commentCard.append(commentText);
+    
+        fragmentComment.append(commentCard);
+        }
+        $(".comments-area").append(fragmentComment);
 
-    fragmentComment.append(commentCard);
-    }
-    $(".comments-area").append(fragmentComment);
+    // End :: Comment-Section
+
+    // }
+
+    // $.ajax({
+    //     type:'GET',
+    //     URL: 'https://mocki.io/v1/8c9b378b-d248-4203-93b0-b8e7659ac346',
+    //     dataType: 'json',
+    //     success: (obj) => {
+    //         posterJsonMap(obj)
+    //     }
+    // });
+
+    // function posterJsonMap(poster){
+
+    // Start :: UpcomingProject-Section 
 
     const postersWrapper = $(`<div class=projectContainer></div>`);
     let fragmentPoster = $(document.createDocumentFragment());
@@ -58,5 +91,9 @@ $.when(
     }
     postersWrapper.append(fragmentPoster);
     $(".upcoming-projects").append(postersWrapper);
+
+    // End :: UpcomingProject-Section 
+
+    // }
 
 })
