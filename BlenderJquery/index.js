f6e4679405aaa3ca8fccb785ac4da7c2964d7ecc
ksakthivel1211 <125632352+ksakthivel1211@users.noreply.github.com>
2023-03-13 19:46:34 +0530

    JqueryBlender

---
+++ BlenderJquery/index.js
@@ -0,0 +1,62 @@
+const posters = "https://mocki.io/v1/8c9b378b-d248-4203-93b0-b8e7659ac346";
+const videos ="https://mocki.io/v1/4da47fc5-bbf3-4e41-b35f-c88a584bc4b0";
+
+$.when(
+    $.getJSON(posters),
+    $.getJSON(videos)
+).done(function(a,b){
+
+    const video = b[0];
+    const poster = a[0];
+
+    let videoElement = $(`<video class=video-element src=${video.videoUrl} poster=https://www.slashcam.de/images/news/sprite_fright1-16857_PIC1.jpg controls></video>`);
+    $(".movie-area").append(videoElement);
+
+    const videoTitle = $(`<h3 class=video-heading></h3>`);
+    videoTitle.append(video.title);
+    $(".movie-area").append(videoTitle);
+
+    const videoDescription = $(`<p class=videoDetails></p>`);
+    videoDescription.append(video.description);
+    $(".movie-area").append(videoDescription);
+
+
+
+    for (const comment of video.comments) {
+
+    const commentCard = $(`<div class=comment-card></div>`);
+    const commentImageWrapper = $(`<div class=imageContainer></div>`);
+    const commentImg = $(`<img class=comment-img src=${comment.image}></img>`)
+    commentImageWrapper.append(commentImg);
+
+    const commentText = $(`<div></div>`);
+
+    const commentHeading = $(`<h2 class=card-heading></h2>`);
+    commentHeading.append(comment.name);
+    
+    const commentDescription = $(`<p class=card-description></p>`);
+    commentDescription.append(comment.comment);
+
+    commentText.append(commentHeading);
+    commentText.append(commentDescription);
+
+    commentCard.append(commentImageWrapper);
+    commentCard.append(commentText);
+
+    $(".comments-area").append(commentCard);
+    }
+
+    const postersWrapper = $(`<div class=projectContainer></div>`);
+    let fragment = $(document.createDocumentFragment());
+
+    for (const project of poster){
+
+        const posterImgWrapper = $(`<div class=projectImage></div>`);
+        const posterImage = $(`<img src=${project.imageUrl}></img>`);
+        posterImgWrapper.append(posterImage);
+        fragment.append(posterImgWrapper);
+    }
+    postersWrapper.append(fragment);
+    $(".upcoming-projects").append(postersWrapper);
+
+})
